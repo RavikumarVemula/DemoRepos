4c05517f1dde481b767d2a52872a29858c9bab1d
Ravi <Ravi123@gmail.com>
2022-06-20 22:48:34 +0530

    First Commit from M1

---
+++ src/test/java/LearningMaven/MavenProj/RESTApiTest.java
@@ -0,0 +1,17 @@
+package LearningMaven.MavenProj;
+
+import org.testng.annotations.Test;
+
+public class RESTApiTest {
+	@Test
+	public void postJira() {
+		System.out.println("postJira1");
+		System.out.println("postJira2");
+		System.out.println("postJira3");
+	}
+	
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
